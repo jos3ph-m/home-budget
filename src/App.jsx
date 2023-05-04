69d23c82964a11e2cdfa7c0e32655ae0f1f01f30
Joseph M <jmedina1985@hotmail.com>
2023-05-04 14:03:01 -0700

    Adding an error route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
     element: <Dashboard/>,
     loader: dashboardLoader,
     errorElement: <Error/>
+  },
+  {
+    path: "*",
+    element: <Error/>
   }
 
 
