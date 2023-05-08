86e64ca1435a70adff9b6b4319fcdd2c5408f055
Joseph M <jmedina1985@hotmail.com>
2023-05-07 20:57:40 -0700

    Layouts and actions

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,15 @@ import {
 // Routes
 import Dashboard, { dashboardLoader } from './pages/Dashboard';
 import Error from './pages/error';
+import Main from './layouts/main';
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main/>,
+    loader: mainLoader,
+    errorElement: <Error/>
+  },
   {
     path: '/',
     element: <Dashboard/>,
