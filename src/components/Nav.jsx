e522f721da38eaf7611107327ed1f2aa6e517b9e
Joseph M <jmedina1985@hotmail.com>
2023-05-12 14:35:03 -0700

    Layouts and actions

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ import logomark from '../assets/logomark.svg'
 const Nav = ({ userName }) => {
   return (
     <nav>
-      <NavLink to="/"><img src={logomark} alt="" height={30}/>
+      <NavLink to="/" aria-label="Go to home"><img src={logomark} alt="" height={30}/>
       <span>HomeBudget</span>
       </NavLink>
       
