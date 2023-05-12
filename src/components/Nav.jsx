43673f34a43f8f31818a5cacfb8b9bad240f3e1d
Joseph M <jmedina1985@hotmail.com>
2023-05-12 14:39:57 -0700

    Layouts and actions

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 // assets
 import logomark from '../assets/logomark.svg'
 
-const Nav = ({ userName }) => {
+export const Nav = ({ userName }) => {
   return (
     <nav>
       <NavLink to="/" aria-label="Go to home"><img src={logomark} alt="" height={30}/>
@@ -15,4 +15,3 @@ const Nav = ({ userName }) => {
   )
 }
 
-export default Nav
