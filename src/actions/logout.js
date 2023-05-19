df7ec24fd6a4965118c9760c768b715db52c5a67
Joseph M <jmedina1985@hotmail.com>
2023-05-19 14:51:47 -0700

    Layouts and actions

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,7 +1,9 @@
 import { redirect } from 'react-router-dom';
+import { deleteItem } from '../helpers';
 
 export async function logoutAction() {
   // delete the user
+  deleteItem;
 
   // return redirect
   return redirect('/');
