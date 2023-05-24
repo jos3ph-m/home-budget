5efc09f91c0f527f723a276fe07b27af4712adcb
Joseph M <jmedina1985@hotmail.com>
2023-05-24 14:43:22 -0700

    Adding a login form

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ function Intro() {
     <div className="intro">
       <div>
         <h1>
-          Take Control of <span className="accent"></span>
+          Take Control of <span className="accent">Your Money</span>
         </h1>
       </div>
     </div>
