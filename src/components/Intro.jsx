1ca57faf798cf82a80a6bd80cd817a28fd79ca54
Joseph M <jmedina1985@hotmail.com>
2023-05-25 10:57:00 -0700

    Adding a login form

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ function Intro() {
     <div className="intro">
       <div>
         <h1>
-          Take Control of <span className="accent">Your $</span>
+          Take Control of <span className="accent">Your Money</span>
         </h1>
         <p>
           Personal budgeting is the secret to financial freedom.  Start your journey today.
