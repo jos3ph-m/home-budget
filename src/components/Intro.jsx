70de9546f371b79d6583d9efa52b0e0ef764f542
Joseph M <jmedina1985@hotmail.com>
2023-05-27 10:35:28 -0700

    Adding a login form

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,7 +11,14 @@ function Intro() {
           Personal budgeting is the gateway to financial freedom.  Start your journey today.
         </p>
         <Form method="post">
-          <input type="text" name="userName" required placeholder='What is your name?' aria-label="Your Name" autoComplete='given-name'/>
+          <input 
+            type="text" 
+            name="userName" 
+            required 
+            placeholder='What is your name?' 
+            aria-label="Your Name" 
+            autoComplete='given-name'
+          />
         </Form>
       </div>
     </div>
