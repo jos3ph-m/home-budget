227b7876ed59d9aac51cde21c8c493c8cea6103c
Joseph M <jmedina1985@hotmail.com>
2023-06-09 21:25:21 -0700

    Small adjustment

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,7 +21,7 @@ export async function dashboardAction({request}) {
 }
 
 const Dashboard = () => {
-  
+  const {userName} = useLoaderData()
 
   return (
     <>{userName ? (<p>{userName}</p>) : <Intro />}Dashboard</>
