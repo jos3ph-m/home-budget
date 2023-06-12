53ec8eb11385511ee5f6839cf4241e66c36a7ae0
Joseph M <jmedina1985@hotmail.com>
2023-06-11 22:04:21 -0700

    Adding a login form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
   const {userName} = useLoaderData()
 
   return (
-    <>{userName ? (<p>{userName}</p>) : <Intro />}Dash</>
+    <>{userName ? (<p>{userName}</p>) : <Intro />}Dashboard</>
   )
 }
 
