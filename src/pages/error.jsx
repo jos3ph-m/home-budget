4373ce2855c3f5283dba51f79e30d025806499d2
Joseph M <jmedina1985@hotmail.com>
2023-06-13 12:00:32 -0700

    Custom error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -2,8 +2,7 @@ import { useRouteError } from 'react-router-dom'
 
 const Error = () => {
   const error = useRouteError();
-  console.log("Error reported: ", error)
-
+  
   return (
     <div>error</div>
   )
