93c3d9b0bb1d28c357009aba53794c6289348d3f
Joseph M <jmedina1985@hotmail.com>
2023-06-21 14:01:26 -0700

    New budget form

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,7 +1,9 @@
 const AddBudgetForm = () => {
   return (
     <div className="form-wrapper">
-      <h2 className="h3"></h2>
+      <h2 className="h3">
+        Create budget
+      </h2>
     </div>
   )
 }
