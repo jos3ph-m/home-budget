e35c5289dde7a6db4c67a144616fa5886c4313e2
Joseph M <jmedina1985@hotmail.com>
2023-06-28 09:57:22 -0700

    New budget form

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -12,6 +12,7 @@ const AddBudgetForm = () => {
           <input type="text" name="newBudget" id="newBudget" placeholder='e.g., Groceries' required/>
         </div>
         <label htmlFor="newBudgetAmount">Amount</label>
+        <input type="number" step="0.01"/>
       </Form>
     </div>
   )
