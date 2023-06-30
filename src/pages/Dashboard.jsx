a81c82441bcf75f2e8c1f833addcf47ac44457b7
Joseph M <jmedina1985@hotmail.com>
2023-06-30 10:05:21 -0700

    Handling multiple form submissions

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -22,12 +22,13 @@ export function dashboardLoader() {
 export async function dashboardAction({request}) {
   const data = await request.formData();
   const {_action, ...values} = Object.fromEntries(data);
-  try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName))
-    return toast.success(`Welcome, ${formData.userName}`)
-  } catch (e) {
-    throw new Error("There was a problem creating your account")
-  }
+  console.log(_action, ...values)
+  // try {
+  //   localStorage.setItem("userName", JSON.stringify(formData.userName))
+  //   return toast.success(`Welcome, ${formData.userName}`)
+  // } catch (e) {
+  //   throw new Error("There was a problem creating your account")
+  // }
   
 }
 
