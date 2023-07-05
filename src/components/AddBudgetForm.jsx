909f3820288462b6522a1d3de79b1c0e5e0b7efc
Joseph M <jmedina1985@hotmail.com>
2023-07-04 17:00:52 -0700

    small change

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -11,13 +11,14 @@ const AddBudgetForm = () => {
       </h2>
       <Form method="post" className='grid-sm'>
         <div className="grid-xs">
-          <label htmlFor="newBudget">Budget Name</label>
+          <label htmlFor="newBudget">Budget</label>
           <input type="text" name="newBudget" id="newBudget" placeholder='e.g., Groceries' required/>
         </div>
         <div className="grid-xs">
         <label htmlFor="newBudgetAmount">Amount</label>
         <input type="number" step="0.01" name="newBudgetAmount" id="newBudgetAmount" placeholder="e.g., $350" required inputMode='decimal'/>
         </div>
+        <input type="hidden" name="_action" value="createBudget"/>
         <button type="submit" className="btn btn--dark">
           <span>Create budget</span>
           <CurrencyDollarIcon width={20}/>
