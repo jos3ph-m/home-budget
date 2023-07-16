0db24067a703d739dce33c937ea10a77568a4c51
Joseph M <jmedina1985@hotmail.com>
2023-07-15 20:52:07 -0700

    New expense form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -57,11 +57,13 @@ const Dashboard = () => {
       <div className="dashboard">
         <h1>Hello again, <span className="accent">{userName}</span></h1>
         <div className="grid-sm">
-          {<div className="grid-lg">
+          {
+            budgets && budgets.length > 0 ?()
+            <div className="grid-lg">
             <div className="flex-lg">
               <AddBudgetForm />
             </div>
-          </div>}
+          </div>):()}
         </div>
       </div>) : <Intro />}
     </>
