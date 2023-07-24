6d9bc6fa617164a338752f9449a1679815e1796b
Joseph M <jmedina1985@hotmail.com>
2023-07-24 13:20:28 -0700

    New expense form

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,7 +1,7 @@
 function AddExpenseForm({budgets}) {
   return (
     <div className="form-wrapper">
-      <h2 className="h3">Add a New <span className="accent">{budgets.length === 1 && `${budgets.map((budg) => budg.name)}:`}</span></h2>
+      <h2 className="h3">Add New <span className="accent">{budgets.length === 1 && `${budgets.map((budg) => budg.name)}:`}</span></h2>
     </div>
   )
 }
