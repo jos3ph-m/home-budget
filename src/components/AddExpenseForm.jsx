03307683832a0311172df168dcc7c82b6d69872b
Joseph M <jmedina1985@hotmail.com>
2023-07-25 10:23:16 -0700

    New expense form

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -2,10 +2,18 @@ import { useFetcher } from 'react-router-dom'
 
 function AddExpenseForm({budgets}) {
   const fetcher = useFetcher()
-  
+
   return (
     <div className="form-wrapper">
-      <h2 className="h3">Add New{" "}<span className="accent">{budgets.length === 1 && `${budgets.map((budg) => budg.name)}:`}</span>{" "}Expense</h2>
+      <h2 className="h3">Add New{" "}
+        <span className="accent">{budgets.length === 1 && `${budgets.map((budg) => budg.name)}:`}
+        </span>
+        {" "}Expense
+      </h2>
+      <fetcher.Form
+      method="post">
+
+      </fetcher.Form>
     </div>
   )
 }
