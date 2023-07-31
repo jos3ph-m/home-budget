70384101d0d5f20006d7aa5ef334ff2d8b5696c1
Joseph M <jmedina1985@hotmail.com>
2023-07-31 16:35:59 -0700

    New expense form

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -26,7 +26,7 @@ function AddExpenseForm({budgets}) {
       >
       <div className='expense-inputs'>
         <div className="grid-xs">
-          <label htmlFor="newExpense">Expense Name:</label>
+          <label htmlFor="newExpense">Expense Name</label>
           <input 
             type="text" 
             name="newExpense"
@@ -37,7 +37,7 @@ function AddExpenseForm({budgets}) {
           />
         </div>
         <div className="grid-xs">
-          <label htmlFor="newExpenseAmount">Amount:</label>
+          <label htmlFor="newExpenseAmount">Amount</label>
           <input 
             type="number"
             step="0.01"
