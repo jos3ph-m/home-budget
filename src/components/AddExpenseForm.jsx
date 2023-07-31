dd82c36ccbad2598c2721ce924428df303bba471
Joseph M <jmedina1985@hotmail.com>
2023-07-30 23:30:27 -0700

    New expense form

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -40,7 +40,7 @@ function AddExpenseForm({budgets}) {
           <label htmlFor="newExpenseAmount">Amount</label>
           <input 
             type="number"
-            step="0.01"
+            step="0.02"
             inputMode='decimal'
             name="newExpenseAmount"
             id="newExpenseAmount"
